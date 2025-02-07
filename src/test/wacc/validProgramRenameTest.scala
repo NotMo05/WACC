564ac13be8579559ba750d31b20d18324996a6f3
Yasin Khan <yak23@ic.ac.uk>
2025-02-07 18:18:23 +0000

    refact: Finish .gitlab-ci.yml; uncomment tests - ts2623, yak23, ap3723, mo623

--- src/test/wacc/validProgramRenameTest.scala
+++ src/test/wacc/validProgramRenameTest.scala
@@ -1,29 +1,29 @@
-// package wacc
+package wacc
 
-// import org.scalatest.flatspec.AnyFlatSpec
-// import org.scalatest.matchers.should.Matchers.*
-// import parsley.{Success, Failure}
-// import scala.io.Source
-// import java.io.File
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
+import parsley.{Success, Failure}
+import scala.io.Source
+import java.io.File
 
-// class validProgramRenamingTest extends AnyFlatSpec {
-//   val directoryPath = "src/test/wacc/wacc-examples/valid/"
-//   val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
+class validProgramRenamingTest extends AnyFlatSpec {
+  val directoryPath = "src/test/wacc/wacc-examples/valid/"
+  val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
 
-//   for (file <- files) {
-//     val fileName = file.getPath
-//     val source = Source.fromFile(file)
-//     val fileContent = source.mkString
-//     source.close()
+  for (file <- files) {
+    val fileName = file.getPath
+    val source = Source.fromFile(file)
+    val fileContent = source.mkString
+    source.close()
 
-//     it should s"successfully parse $fileName" in {
-//       parser.parse(fileContent) match {
-//         case Success(ast) => {
-//           val (newProg, errors) = rename(ast)
-//           assert(errors.isEmpty)
-//         }
-//         case Failure(msg) => fail(s"$msg?")
-//       }
-//     }
-//   }
-// }
+    it should s"successfully parse $fileName" in {
+      parser.parse(fileContent) match {
+        case Success(ast) => {
+          val (newProg, errors) = rename(ast)
+          assert(errors.isEmpty)
+        }
+        case Failure(msg) => fail(s"$msg?")
+      }
+    }
+  }
+}
--- src/test/wacc/validProgramSyntaxTest.scala
+++ src/test/wacc/validProgramSyntaxTest.scala
@@ -1,26 +1,26 @@
-// package wacc
+package wacc
 
-// import org.scalatest.flatspec.AnyFlatSpec
-// import org.scalatest.matchers.should.Matchers.*
-// import parsley.{Success, Failure}
-// import scala.io.Source
-// import java.io.File
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
+import parsley.{Success, Failure}
+import scala.io.Source
+import java.io.File
 
-// class validProgramSyntaxTest extends AnyFlatSpec {
-//   val directoryPath = "src/test/wacc/wacc-examples/valid/"
-//   val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
+class validProgramSyntaxTest extends AnyFlatSpec {
+  val directoryPath = "src/test/wacc/wacc-examples/valid/"
+  val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
 
-//   for (file <- files) {
-//     val fileName = file.getPath
-//     val source = Source.fromFile(file)
-//     val fileContent = source.mkString
-//     source.close()
+  for (file <- files) {
+    val fileName = file.getPath
+    val source = Source.fromFile(file)
+    val fileContent = source.mkString
+    source.close()
 
-//     it should s"successfully parse $fileName" in {
-//       parser.parse(fileContent) match {
-//         case Success(_) =>
-//         case Failure(msg) => fail(s"$msg")
-//       }
-//     }
-//   }
-// }
+    it should s"successfully parse $fileName" in {
+      parser.parse(fileContent) match {
+        case Success(_) =>
+        case Failure(msg) => fail(s"$msg")
+      }
+    }
+  }
+}

2d9f32f54b4502c84d66b225523cb76a89ff4df8
Mohamed Omar <mo623@ic.ac.uk>
2025-02-25 15:32:07 +0000

    feat: add scaffolding for labels and instructions to an asm file mo623 yak23

--- src/main/wacc/back_end/AssemblyWriter.scala
+++ src/main/wacc/back_end/AssemblyWriter.scala
@@ -1,5 +1,46 @@
 package wacc.back_end
 
+import instrument._
+
 object AssemblyWriter {
-  def generateAsmFile(ir: (List[Section], List[LabelDef])) = ???
+  val finalAssembly = StringBuilder()
+
+  def generateAsmFile(ir: (List[Section], List[LabelDef])) = {
+
+
+    ir._2.map(labelHandler(_))
+    
+  }
+
+  def labelHandler(labelDef: LabelDef): Unit = {
+    labelDef match 
+      case FuncLabelDef(name, instructions, localLabelDefs) => {
+        finalAssembly.append(s"$name:")
+        instructions.result.map(instructionHandler(_))
+        localLabelDefs.result.map(labelHandler(_))
+      }
+      case wacc.back_end.LocalLabelDef(_, _) => ??? 
+  }
+
+
+  def instructionHandler(instr: Instr) = {
+    instr match
+      case ADD(ops) =>  ???
+      case SUB(ops) => ???
+      case IDIV(op) => ???
+      case IMUL(ops) => ???
+      case AND(ops) => ???
+      case OR(ops) => ???
+      case XOR(ops) => ???
+      case NEG(op) => ???
+      case NOT(op) => ???
+      case MOV(op) => ???
+      case PUSH(op) => ???
+      case POP(op) => ???
+      case CMP(ops) => ???
+      case SETCond(cond, op) => ???
+      case RET() => ???
+      case CALL(op) => ???
+      case CDQ => ???
+  }
 }

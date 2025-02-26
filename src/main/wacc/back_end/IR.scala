fc325f738a3fc6f4840fb228a9b4722a32ff50a0
Mohamed Omar <mo623@ic.ac.uk>
2025-02-26 01:27:39 +0000

    feat: Add fst/snd assigns, add Array assigns, add builder appending yak23

--- src/main/wacc/back_end/IR.scala
+++ src/main/wacc/back_end/IR.scala
@@ -4,11 +4,15 @@ import wacc.front_end._
 import RegName._
 import DataWidth._
 import ExprGen._
+import wacc.back_end.Stack.typeToSize
+import scala.collection.mutable.{Builder}
+
 // At the start of each assembly file
 // .intel_syntax noprefix
 // .globl main
-object IR {
 
+object IR {
+  val STACK_ALIGN = -16
   def generateIR(prog: Prog): (List[Section], List[LabelDef]) = {
     ???
   }
@@ -17,6 +21,7 @@ object IR {
   val labelDefs = List.newBuilder[LabelDef] // each function may contain labels/jumping points (ie for loops)
 
   def generateSection(prog: Prog) = {
+    ???
     // This will generate the boilerplate at the beginning of the asm file and
     // the string+len of string stuff to go in that section
   }
@@ -26,250 +31,129 @@ object IR {
       mainGenerate(main)
   }
 
-  def mainGenerate(stmts: List[Stmt]) = {
-
+  def mainGenerate(stmts: List[Stmt]): Builder[Instr, List[Instr]] = {
+    val instructionBuilder = List.newBuilder[Instr]
+    Stack.initialise(stmts)
+    println(Stack.frames.last.identTable)
+    stmts.map(stmtGen(_, instructionBuilder))
+    return instructionBuilder
   }
 
-  def stmtGen(stmt: Stmt): List[Instr] = stmt match {
+  def stmtGen(stmt: Stmt, builder: Builder[Instr, List[Instr]]) = stmt match {
     case Skip => Nil
     case Read(lValue) => ???
     case Free(expr) => ???
     case Return(expr) => ???
-    case Exit(expr) =>
-      List(
-        PUSH(Reg(Rbp, DWord)),
-        PUSH(Reg(Rbx, DWord)),
-        MOV(Reg(Rbp, DWord), Reg(Rsp, DWord))) ++
-        exprGen(expr, 10) ++
-        List(MOV(Reg(Rdi, DWord), Reg(R10)))
-        // CALL(Label())
-
+    case Exit(expr) => {
+      exprGen(expr, builder)
+      builder.addAll(
+        List(
+          MOV(Reg(Rdi, DWord), Reg(R10)),
+          AND(Reg(Rsp), Imm(STACK_ALIGN)),
+          CALL(Label("exit@plt"))
+        )
+      )
+    }
     case Print(expr) => ???
     case Println(expr) => ???
     case WhileDo(condition, stmts) => ???
     case IfElse(condition, thenStmts, elseStmts) => ???
-    case Assgn(t, identifier: QualifiedName, rValue) => assgnGen(t, identifier, rValue)
-    case ReAssgn(lValue, rValue) => ???
+    case Assgn(t, identifier: QualifiedName, rValue) => assgnGen(t, identifier, rValue, builder)
+    case ReAssgn(lValue, rValue) => reassgnGen(lValue, rValue, builder)
     case Scope(stmts) => ???
   }
 
-  def assgnGen(t: Type, identifier: QualifiedName, rValue: RValue) : List[Instr] = {
+  def assgnGen(t: Type, identifier: QualifiedName, rValue: RValue, builder: Builder[Instr, List[Instr]]) : Builder[Instr, List[Instr]] = {
+    val offset = Stack.frames.last.identTable(identifier)
+    val memSize = Stack.typeToSize(t)
+
     rValue match
-      case expr: Expr => ???
+      case expr: Expr => {
+        exprGen(expr, builder)
+        builder += MOV(OffsetAddr(Some(memSize), Reg(Rbp), offset), Reg(10))
+      }
+
+      case ArrayLiter(elems) => {
+        val arrayTypeSize = typeToSize(t.asInstanceOf[ArrayType].t)
+        builder.addAll(
+          List(
+            MOV(Reg(Rdi), Imm(arrayTypeSize*(elems.size) + 4)),
+            CALL(Label("_malloc")),
+            MOV(OffsetAddr(Some(memSize), Reg(Rax)), Imm(elems.size)),
+            ADD(Reg(Rax), Imm(4))
+            )
+        )
+        elems.indices.map(i => {
+          exprGen(elems(i), builder)
+          builder += MOV(OffsetAddr(Some(arrayTypeSize), Reg(Rax), i*arrayTypeSize), Reg(R10))
+        })
+        builder += MOV(OffsetAddr(Some(memSize), Reg(Rbp), offset), Reg(Rax))
+      }
+
+      case NewPair(fst, snd) => {
+        // Need to deal with the case fst or snd are a pair
+        val (fstSize, sndSize) = (8, 8)
+        builder.addAll(
+          List(
+            MOV(Reg(Rdi), Imm(fstSize+sndSize)),
+            CALL(Label("_malloc")),
+            MOV(OffsetAddr(Some(memSize), Reg(Rbp), offset), Reg(Rax))
+            )
+        )
+          exprGen(fst, builder)
+          builder += MOV(OffsetAddr(Some(fstSize), Reg(Rax)),Reg(R10))
+          exprGen(snd, builder)
+          builder += MOV(OffsetAddr(Some(sndSize), Reg(Rax), sndSize),Reg(R10))
+        
+      } 
+
+
+      case Fst(lValue) => {
+        fstSndHandle(lValue, builder)
+        builder += MOV(OffsetAddr(Some(8), Reg(Rbp), 8), Reg(R9))
+      }
+      case Snd(lValue) => {
+        fstSndHandle(lValue, builder, 8)
+        builder += MOV(OffsetAddr(Some(8), Reg(Rbp), 8), Reg(R9))
+      }
       case Call(ident, args) => ???
-      case ArrayLiter(elems) => ???
-      case NewPair(fst, snd) => ??? 
-      case Fst(lValue) => ???
-      case Snd(lValue) => ???
+  }
+  
+  def reassgnGen(lValue: LValue, rValue: RValue, builder: Builder[Instr, List[Instr]]) = {
+    lValue match
+        case qn: QualifiedName => assgnGen(qn.t, qn, rValue, builder)
+        case ArrayElem(arrayName, index) => ???
+        case Fst(lValue) => ???
+        case Snd(lValue) => ???
   }
 
-
-
+  def fstSndHandle(lValue: LValue, builder: Builder[Instr, List[Instr]], fstOrSnd: Int = 0): List[Instr] = {
+    lValue match 
+      case qn: QualifiedName => {
+        val pairOffset = Stack.frames.last.identTable(qn)
+        List(
+        MOV(Reg(9), OffsetAddr(Some(MemOpModifier.QWordPtr), Reg(Rbp), pairOffset)),
+        CMP(Reg(9), Imm(0)),
+        JCond(Cond.E, Label("_errNull")),
+        MOV(Reg(9), OffsetAddr(Some(MemOpModifier.QWordPtr), Reg(Rbp), pairOffset + fstOrSnd)))
+      }
+      case ArrayElem(arrayName, index) => ???
+      case Fst(lValue) => {
+        fstSndHandle(lValue, builder) ++
+        List(
+        CMP(Reg(9), Imm(0)),
+        JCond(Cond.E, Label("_errNull")),
+        MOV(Reg(9), OffsetAddr(Some(MemOpModifier.QWordPtr), Reg(9), fstOrSnd))
+        )
+      }
+      case Snd(lValue) => {
+        fstSndHandle(lValue, builder, 8) ++
+        List(
+        CMP(Reg(9), Imm(0)),
+        JCond(Cond.E, Label("_errNull")),
+        MOV(Reg(9), OffsetAddr(Some(MemOpModifier.QWordPtr), Reg(9), fstOrSnd))
+        )
+      }
+  }
 }
 
-// Prog(
-//   List(
-//     Func(
-//       IntType,
-//       QualifiedFunc(
-//         t=IntType,
-//         funcName=fibonacci,
-//         paramNum=1,
-//         paramTypes=List(
-//           IntType
-//         )
-//       ),
-//       List(
-//         Param(
-//           IntType,
-//           QualifiedName(
-//             name=n,
-//             num=0,
-//             t=IntType
-//           )
-//         )
-//       ),
-//       List(
-//         IfElse(
-//           LessE(
-//             QualifiedName(
-//               name=n,
-//               num=0,
-//               t=IntType
-//             ),
-//             IntLiteral(
-//               1
-//             )
-//           ),
-//           List(
-//             Return(
-//               QualifiedName(
-//                 name=n,
-//                 num=0,
-//                 t=IntType
-//               )
-//             )
-//           ),
-//           List(
-//             Skip
-//           )
-//         ),
-//         Assgn(
-//           IntType,
-//           QualifiedName(
-//             name=f1,
-//             num=0,
-//             t=IntType
-//           ),
-//           Call(
-//             QualifiedFunc(
-//               t=IntType,
-//               funcName=fibonacci,
-//               paramNum=1,
-//               paramTypes=List(
-//                 IntType
-//               )
-//             ),
-//             List(
-//               Sub(
-//                 QualifiedName(
-//                   name=n,
-//                   num=0,
-//                   t=IntType
-//                 ),
-//                 IntLiteral(
-//                   1
-//                 )
-//               )
-//             )
-//           )
-//         ),
-//         Assgn(
-//           IntType,
-//           QualifiedName(
-//             name=f2,
-//             num=0,
-//             t=IntType
-//           ),
-//           Call(
-//             QualifiedFunc(
-//               t=IntType,
-//               funcName=fibonacci,
-//               paramNum=1,
-//               paramTypes=List(
-//                 IntType
-//               )
-//             ),
-//             List(
-//               Sub(
-//                 QualifiedName(
-//                   name=n,
-//                   num=0,
-//                   t=IntType
-//                 ),
-//                 IntLiteral(
-//                   2
-//                 )
-//               )
-//             )
-//           )
-//         ),
-//         Return(
-//           Add(
-//             QualifiedName(
-//               name=f1,
-//               num=0,
-//               t=IntType
-//             ),
-//             QualifiedName(
-//               name=f2,
-//               num=0,
-//               t=IntType
-//             )
-//           )
-//         )
-//       )
-//     )
-//   ),
-//   List(
-//     Println(
-//       StringLiteral(
-//         Thisprogramcalculatesthenthfibonaccinumberrecursively.
-//       )
-//     ),
-//     Print(
-//       StringLiteral(
-//         Pleaseentern(
-//           shouldnotbetoolarge
-//         ):
-//       )
-//     ),
-//     Assgn(
-//       IntType,
-//       QualifiedName(
-//         name=n,
-//         num=1,
-//         t=IntType
-//       ),
-//       IntLiteral(
-//         0
-//       )
-//     ),
-//     Read(
-//       QualifiedName(
-//         name=n,
-//         num=1,
-//         t=IntType
-//       )
-//     ),
-//     Print(
-//       StringLiteral(
-//         Theinputnis
-//       )
-//     ),
-//     Println(
-//       QualifiedName(
-//         name=n,
-//         num=1,
-//         t=IntType
-//       )
-//     ),
-//     Print(
-//       StringLiteral(
-//         Thenthfibonaccinumberis
-//       )
-//     ),
-//     Assgn(
-//       IntType,
-//       QualifiedName(
-//         name=result,
-//         num=0,
-//         t=IntType
-//       ),
-//       Call(
-//         QualifiedFunc(
-//           t=IntType,
-//           funcName=fibonacci,
-//           paramNum=1,
-//           paramTypes=List(
-//             IntType
-//           )
-//         ),
-//         List(
-//           QualifiedName(
-//             name=n,
-//             num=1,
-//             t=IntType
-//           )
-//         )
-//       )
-//     ),
-//     Println(
-//       QualifiedName(
-//         name=result,
-//         num=0,
-//         t=IntType
-//       )
-//     )
-//   )
-// )

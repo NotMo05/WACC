d48d8f50e7c812ece0508a697ada26b69ad891ca
adiprabs <ap3723@ic.ac.uk>
2025-02-27 15:25:56 +0000

    feat: Added testing feature
    Tests now create binaries from assembly file, and verify output from original wacc file

--- src/test/wacc/back_end_tests/produceAssembly.scala
+++ src/test/wacc/back_end_tests/produceAssembly.scala
@@ -10,8 +10,21 @@ import wacc.back_end.IR
 import wacc.back_end.AssemblyWriter
 import wacc.back_end.IR.generate
 import wacc.back_end.AssemblyWriter.generateAsmFile
+import scala.sys.process._
 
 class produceAssembly extends AnyFlatSpec {
+
+  def findOutputValue(filePath: String): Option[String] = {
+    val source = Source.fromFile(filePath)
+    try {
+      source.getLines()
+        .find(_.startsWith("output:")) // Find the first line starting with "output:"
+        .map(_.stripPrefix("output:").trim) // Remove "output:" and trim whitespace
+    } finally {
+      source.close()
+    }
+  }
+
   val directoryPath = "src/test/wacc/wacc-examples/valid/"
   val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
 
@@ -28,10 +41,39 @@ class produceAssembly extends AnyFlatSpec {
         // pending
         parser.parse(fileContent) match {
           case Success(ast) => {
+
             val (newProg, errors) = rename(ast)
             assert(errors.isEmpty && semantic.analyse(newProg).isEmpty)
             val mainLabel = generate(newProg)
             generateAsmFile((List(), List(mainLabel)), fileName, "assemblyFiles/")
+
+            //TODO GET ASSEMBLY FILE PATH
+            val baseName = new File(fileName).getName.replace(".wacc", "")
+            val assemblyFilepath = s"assemblyFiles/${baseName}.s"
+
+            val compileProcess = Process(s"gcc -o output_binary ${assemblyFilepath}")
+            val compileExitCode = compileProcess.!
+            
+            var output = new StringBuilder
+            var errorOutput = new StringBuilder
+
+            val logger = ProcessLogger(
+              (out: String) => output.append(out + "\n"),  // Capture stdout
+              (err: String) => errorOutput.append(err + "\n") // Capture stderr
+            )
+
+            val exitCode = Process("./output_binary").!(logger)
+
+            println(s"Program output:\n$output")
+            println(s"Error output:\n$errorOutput")
+            
+            val expectedResult = findOutputValue(assemblyFilepath) 
+
+            if (expectedResult.contains(output.toString())) {
+              println("Output matches result")
+            } else {
+              println(s"expected ${value} but got ${output}")
+            }
           }
           case Failure(msg) => fail(s"$msg?")
         }

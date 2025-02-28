b091387c10b3b5ea96035ff5df1c65e9e1e8a191
Yasin Khan <yak23@ic.ac.uk>
2025-02-28 11:12:16 +0000

    test: set expression tests to pending, set basic tests to run

--- src/test/wacc/back_end_tests/produceAssembly.scala
+++ src/test/wacc/back_end_tests/produceAssembly.scala
@@ -26,10 +26,10 @@ class produceAssembly extends AnyFlatSpec {
         .map(_.stripPrefix("#").trim) // Remove "#" and trim whitespace
         .map(line => "  " + line)
         .toList
-      
+
       // Find index of "Output:" marker
       val outputIndex = lines.indexWhere(_.contains("Output:"))
-      
+
       if (outputIndex >= 0) {
         lines.drop(outputIndex + 1) // Take all lines after "Output:"
           .filter(!_.contains("Program:")) // Filter out lines containing "Program:"
@@ -43,75 +43,61 @@ class produceAssembly extends AnyFlatSpec {
     }
   }
 
-  val directoryPath = "src/test/wacc/wacc-examples/valid/expressions"
-  val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
+    /** Function to dynamically register tests for a given folder **/
+  def runTest(directoryPath: String, isPending: Boolean): Unit = {
+    val files = FileUtils.listAllFiles(new File(directoryPath)).filter(_.isFile)
 
-
-  for (file <- files) {
-    
-    val fileName = file.getPath
-
-    if (fileName.endsWith(".wacc")) {
-      val source = Source.fromFile(file)
-      val fileContent = source.mkString
-      source.close()
+    for (file <- files if file.getPath.endsWith(".wacc")) {
+      val fileName = file.getPath
 
       it should s"successfully produce assembly for $fileName" in {
-        // pending
-        parser.parse(fileContent) match {
-          case Success(ast) => {
+        if isPending then pending
+        val source = Source.fromFile(file)
+        val fileContent = try source.mkString finally source.close()
 
+        parser.parse(fileContent) match {
+          case Success(ast) =>
             val (newProg, errors) = rename(ast)
             assert(errors.isEmpty && semantic.analyse(newProg).isEmpty)
-            val IR = generateIR(newProg)
-            generateAsmFile(IR, fileName, s"${assDir}/")
-            
-            val baseName = new File(fileName).getName.replace(".wacc", "")
-            val assemblyFilepath = s"${assDir}/${baseName}.s"
-            
-            
 
-            val binDirPath = new File(binDir)
-            if (!binDirPath.exists()) {
-              binDirPath.mkdirs()
+            val IR = generateIR(newProg)
+            generateAsmFile(IR, fileName, s"$assDir/")
+
+            val baseName = new File(fileName).getName.replace(".wacc", "")
+            val assemblyFilepath = s"$assDir/$baseName.s"
+
+            if (!new File(binDir).exists()) {
+              new File(binDir).mkdirs()
             }
 
-            val compilation = Process(s"gcc -z noexecstack -o ${binDir}/${baseName} ${assemblyFilepath}").!
-            println(s"\nCompilation exit code: ${compilation}")
-            
-            
-            // println(s"Output binary exists?: ${File(s"./${binDir}/${baseName}").exists()}")
-            // println(s"assembly file path is ${assemblyFilepath}")
-            
-            val exitCode = Process(s"./${binDir}/$baseName").!
-            // val output = Process(s"./${binDir}/$baseName").!!
-            
-            println(s"Program exitCode: $exitCode")
-            // println(s"Program output: $output")
-            
-            // println(s"Original Wacc file ?: ${File(s"${file.getPath}").exists()}")
-            val expectedResult = findOutputValues(file.getPath) 
+            val compilation = Process(s"gcc -z noexecstack -o $binDir/$baseName $assemblyFilepath").!
+            println(s"\nCompilation exit code: $compilation")
 
+            val exitCode = Process(s"./$binDir/$baseName").!
+            println(s"Program exitCode: $exitCode")
+
+            val expectedResult = findOutputValues(file.getPath)
             println(s"Comments are: $expectedResult")
 
             if (expectedResult.isEmpty) {
               println("Not expecting any exit codes or returns")
               assert(exitCode.toString == "0")
-            } else if (expectedResult(0) == "Exit:") {
-              val expected = expectedResult(1) 
+            } else if (expectedResult.head == "Exit:") {
+              val expected = expectedResult(1)
               println(s"Expecting exit code of $expected")
               assert(expected == exitCode.toString)
             }
 
-            // if (expectedResult.contains(output.toString())) {
-            //   println("Output matches result")
-            // } else {
-            //   println(s"expected ${value} but got ${output}")
-            // }
-          }
-          case Failure(msg) => fail(s"$msg?")
+          case Failure(msg) => fail(s"Parsing failed: $msg")
         }
       }
     }
   }
+
+
+  val expressionPath = "src/test/wacc/wacc-examples/valid/expressions"
+  val basicPath = "src/test/wacc/wacc-examples/valid/basic"
+
+  runTest(expressionPath, true)
+  runTest(basicPath, false)
 }
--- src/test/wacc/front_end_tests/validProgramSyntaxTest.scala
+++ src/test/wacc/front_end_tests/validProgramSyntaxTest.scala
@@ -16,7 +16,7 @@ class validProgramSyntaxTest extends AnyFlatSpec {
     val fileContent = source.mkString
     source.close()
 
-    ignore should s"successfully parse $fileName" in {
+    it should s"successfully parse $fileName" in {
       parser.parse(fileContent) match {
         case Success(_) =>
         case Failure(msg) => fail(s"$msg")
